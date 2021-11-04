3ff0c5802d8bccf996bc925f9f83eb114a7f4552
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2021-11-04 18:57:03 -0400

    example: Update to include required_providers

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,18 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "cloudflare" {
-  version = "~> 2.0"
   email   = var.cloudflare_email
   api_key = var.cloudflare_token
 }
